af0a72e86813cac9932ccbc41ca538c4bd45b3a1
Ellie <intricatebread@gmail.com>
2025-12-26 00:44:58 -0700

    update to new NBT

--- src/Litematica.jl
+++ src/Litematica.jl
@@ -1,14 +1,16 @@
 module Litematica
 
 using NBT, MinecraftDataStructures, PooledArrays
+using OrderedCollections
+using BufferedStreams, CodecZlib
 
 export Region, Litematic
 
 struct Region{T<:AbstractBlockState, I<:Unsigned}
-  name::String
-  pos::Tuple{Int32, Int32, Int32}
+  # name::String
+  position::Tuple{Int32, Int32, Int32}
   blocks::PooledArray{T, I, 3, Array{I, 3}}
-  tile_entities::Array{Union{TagCompound, Nothing}, 3}
+  tileEntities::Array{Union{LittleDict{String}, Nothing}, 3}
 end
 
 Base.isequal(x::Region, y::Region) = x.name == y.name && x.pos == y.pos && x.blocks == y.blocks && x.tile_entities == y.tile_entities
@@ -23,13 +25,15 @@ end
 
 @inline function Region(blocks::PooledArray{<:AbstractBlockState, UInt32, 3, Array{UInt32, 3}})
   is_air(blocks.pool[1]) || ArgumentError("Litematica regions must have air as the first palette element.")
-  return Region("region", Int32.((0, 0, 0)), blocks, reshape(Union{TagCompound, Nothing}[], 0, 0, 0))
+  return Region("region", Int32.((0, 0, 0)), blocks, reshape(Union{LittleDict{String}, Nothing}[], 0, 0, 0))
 end
 
 struct Litematic
-  data_version::Int32
-  metadata::TagCompound
-  regions::Vector{Region}
+  minecraftDataVersion::Int32
+  version::Int32
+  metadata::LittleDict{String}
+  # regions::Vector{Region}
+  regions::LittleDict{String, Region}
 end
 
 Base.isequal(x::Litematic, y::Litematic) = x.data_version == y.data_version && x.metadata == y.metadata && x.regions == y.regions
@@ -39,136 +43,214 @@ Base.hash(l::Litematic, h::UInt) = hash(l.data_version, hash(l.metadata, hash(l.
 @inline Litematic(blocks::Array{<:AbstractBlockState, 3}) = Litematic(Region(blocks))
 @inline Litematic(blocks::PooledArray{<:AbstractBlockState, UInt32, 3, Array{UInt32, 3}}) = Litematic(Region(blocks))
 @inline Litematic(region::Region) = Litematic([region])
-@inline Litematic(regions::Vector{Region{A, B}}) where {A,B} = Litematic(2586, TagCompound(), regions)
+@inline Litematic(regions::Vector{Region{A, B}}) where {A,B} = Litematic(2586, LittleDict{String}(), regions)
 
 function Base.show(io::IO, M::MIME"text/plain", lr::Region)
-  println(io, "Region \"", lr.name, "\" at ", lr.pos, ':')
+  # println(io, "Region \"", lr.name, "\" at ", lr.pos, ':')
+  println(io, "Region at ", lr.position, ':')
   show(io, M, lr.blocks)
 end
 
 function Base.show(io::IO, lr::Region)
-  a = lr.pos
-  b = lr.pos .+ size(lr.blocks) .- 1
-  print(io, '"', lr.name, "\" (", a[1], ' ', a[2], ' ', a[3], " ~ ",
+  a = lr.position
+  b = lr.position .+ size(lr.blocks) .- 1
+  # print(io, '"', lr.name, "\" (", a[1], ' ', a[2], ' ', a[3], " ~ ",
+  #       b[1], ' ', b[2], ' ', b[3], ')')
+  print(io, "Region(", a[1], ' ', a[2], ' ', a[3], " ~ ",
         b[1], ' ', b[2], ' ', b[3], ')')
 end
 
-# function _readtriple2(io::IO, V::Val{0xa})
-#   return NBT._read_tag(io, V, dict2)
-# end
-#
-# function _readproperties(io::IO, V::Val{0xa})
-#   return NBT._read_tag(io, V, _readproperty, Pair{String, String})
-# end
-#
-# function _readblockstatepalette(io::IO, V::Val{0x9})
-#   return NBT._read_tag(io, V, _readAbstractBlockState, AbstractBlockState)
-# end
-#
-# function _readregions(io::IO, V::Val{0xa})
-#   return NBT._read_tag(io, V, _readregion, NamedTuple)
-# end
-#
-# "Properties" => (:props => _readproperties)
-#
-# "Position" => (:position => _readtriple2),
-# "Size" => (:size => _readtriple2),
-# "BlockStatePalette" => (:palette => _readblockstatepalette),
-#
-# "Regions" => (:regions => _readregions)
+function bitunpack(compressed::Vector{Int64}, palette::Vector{T}, min_bits::Int64, size::NTuple{3, Int32}) where T<:AbstractBlockState
+  wordsize = max(min_bits, ceil(Int, log2(length(palette))))
+  data = ones(UInt32, size)
+  shift = 0
+  j = 1
+  mask = 2^wordsize - 1
+  for i in 1:prod(size)
+    data[i] += (compressed[j] >>> shift) & mask
+    if shift + wordsize > 64
+      data[i] += (compressed[j += 1] >>> (shift - 64)) & mask
+      shift = shift + wordsize - 64
+    elseif shift + wordsize == 64
+      j += 1
+      shift = 0
+    else
+      shift += wordsize
+    end
+  end
+  return PooledArray(PooledArrays.RefArray(data), Dict{T, UInt32}(b => UInt32(i) for (i, b) in enumerate(palette)), palette, Threads.Atomic{Int64}(1))
+end
 
-#---------------------------------------------------------------------------------------------------------------------
-
-# _read_tag(N::UInt8, dict::Dict{String, Pair{Symbol, Function}}) = _read_tag(Val(N), dict)
-# function _read_tag(::Val{N}, dict::Dict{String, Pair{Symbol, Function}}) where N
-#   return (io::IO, V::Val{N}) -> NBT._read_tag(io, V, dict)
-# end
-#
-# _read_tag(N::UInt8, ::Type{T}, f::Function) where T = _read_tag(Val(N), T, f)
-# function _read_tag(::Val{N}, ::Type{T}, f::Function) where {N, T}
-#   return (io::IO, V::Val{N}) -> NBT._read_tag(io, V, f, T)
-# end
-#
-# function _readregion(io::IO, V::Val{0xa}, name::String)
-#   data = NBT._read_tag(io, V, dict1)
-#   pos = (data.position.x, data.position.y, data.position.z)
-#   size_ = (data.size.x, data.size.y, data.size.z)
-#   pos = min.(pos, pos .+ size_ .- sign.(size_)) # Get negativemost corner
-#   size_ = Int64.(abs.(size_)) # Fix size_ sign
-#
-#   # Array of size `size_`, but reading blocks in XZY order
-#   compressedBlocks = CompressedPalettedContainer(data.palette, data.bs)
-#   blocks = PooledArray(compressedBlocks, 2, (size_[1], size_[3], size_[2]))
-#   blocks.refs = permutedims(blocks.refs, (1, 3, 2))
-#
-#   tile_entities::Array{Union{TagCompound, Nothing}} = fill(nothing, size_...)
-#   for te in data.te
-#     x, y, z = _readtriple(te)
-#     tile_entities[x+1, y+1, z+1] = te
-#   end
-#
-#   return Region(name, pos, blocks, tile_entities)
-# end
-#
-# function _readAbstractBlockState(io::IO, V::Val{0xa})
-#   data = NBT._read_tag(io, V, dict3)
-#   return haskey(data, :props) ? BlockStateVector(data.name, data.props) : Block(data.name)
-# end
-#
-# function _readproperty(io::IO, V::Val{0x8}, name::String)
-#   return name => NBT._read_tag(io, V)
-# end
-#
-# const dict3 = Dict{String, Pair{Symbol, Function}}(
-#   "Name" => (:name => NBT._read_tag),
-#   "Properties" => (:props => _read_tag(0xa, Pair{String, String}, _readproperty))
-# )
-# const dict2 = Dict{String, Pair{Symbol, Function}}(
-#   "x" => (:x => NBT._read_tag),
-#   "y" => (:y => NBT._read_tag),
-#   "z" => (:z => NBT._read_tag)
-# )
-# const dict1 = Dict{String, Pair{Symbol, Function}}(
-#   "BlockStates" => (:bs => NBT._read_tag),
-#   "TileEntities" => (:te => NBT._read_tag),
-#   "Position" => (:position => _read_tag(0xa, dict2)),
-#   "Size" => (:size => _read_tag(0xa, dict2)),
-#   "BlockStatePalette" => (:palette => _read_tag(0x9, AbstractBlockState, _readAbstractBlockState)),
-# )
-# const dict = Dict{String, Pair{Symbol, Function}}(
-#   "MinecraftDataVersion" => (:dataversion => NBT._read_tag),
-#   "Metadata" => (:metadata => NBT._read_tag),
-#   "Regions" => (:regions => _read_tag(0xa, NamedTuple, _readregion))
-# )
+# TODO: this is hella slow
+function bitpack(uncompressed::PooledArray{<:AbstractBlockState}, min_bits::Int)
+  wordsize = max(min_bits, ceil(Int, log2(length(uncompressed.pool))))
+  reinterpret.(Int64, BitArray((n - 1) >>> i & 1 == 1 for n in uncompressed.refs for i in 0:wordsize - 1).chunks)
+end
 
 function Base.read(io::IO, ::Type{Litematic})
-  root_tag = read(io, TagCompound).second
-  regiontags = root_tag["Regions"].data
-  regions = Vector{Region}(undef, length(regiontags))
-
-  for (i, pair) in enumerate(regiontags)
-    regiontag = pair.second
-    pos, size_ = _readtriple.((regiontag["Position"], regiontag["Size"]))
-    pos = min.(pos, pos .+ size_ .- sign.(size_)) # Get negativemost corner
-    size_ = Int64.(abs.(size_)) # Fix size_ sign
-
-    # Array of size `size_`, but reading blocks in XZY order
-    palette = _AbstractBlockState.(regiontag["BlockStatePalette"].data)
-    compressedBlocks = CompressedPalettedContainer(palette, regiontag["BlockStates"])
-    blocks = PooledArray(compressedBlocks, 2, (size_[1], size_[3], size_[2]))
-    blocks.refs = permutedims(blocks.refs, (1, 3, 2))
-
-    tile_entities::Array{Union{TagCompound, Nothing}} = fill(nothing, size_...)
-    for te in regiontag["TileEntities"].data
-      x, y, z = _readtriple(te)
-      tile_entities[x+1, y+1, z+1] = te
+  stream = BufferedInputStream(GzipDecompressorStream(io))
+  skip(stream, 1) # Skip 0x0a
+  skip(stream, ntoh(read(stream, UInt16))) # Skip name
+  local minecraftDataVersion::Int32, version::Int32, metadata::LittleDict{String}, regions::LittleDict{String, Region}
+  while (contentsid = read(stream, UInt8)) !== 0x0
+    name = NBT._read_name(stream)
+    if contentsid == 0x3 && name == "MinecraftDataVersion"
+      minecraftDataVersion = NBT._read_tag3(stream)
+    elseif contentsid == 0x3 && name == "Version"
+      version = NBT._read_tag3(stream)
+    elseif contentsid == 0xa && name == "Metadata"
+      metadata = NBT._read_taga(stream)
+    elseif contentsid == 0xa && name == "Regions"
+      names = String[]
+      data = Region[]
+      while (contentsid = read(stream, UInt8)) !== 0x0
+        push!(names, NBT._read_name(stream))
+        push!(data, read_region(stream))
+      end
+      regions = LittleDict(names, data)
+    else
+      NBT._lut_skip[contentsid](io)
     end
+  end
+  return Litematic(minecraftDataVersion, version, metadata, regions)
+end
 
-    regions[i] = Region(pair.first, pos, blocks, tile_entities)
+function read_region(io::IO)
+  local position::NTuple{3, Int32}
+  local size_::NTuple{3, Int32}
+  local tileEntities::Vector{LittleDict{String, Any}}
+  local palette::Vector{AbstractBlockState}
+  local blockStates::Vector{Int64}
+  while (contentsid = read(io, UInt8)) !== 0x0
+    name = NBT._read_name(io)
+    if contentsid == 0xc && name == "BlockStates"
+      blockStates = NBT._read_tagc(io)
+    elseif contentsid == 0xa && name == "Position"
+      position = read_coords(io)
+    elseif contentsid == 0x9 && name == "BlockStatePalette"
+      palette = read_blockstate_palette(io)
+    elseif contentsid == 0xa && name == "Size"
+      size_ = read_coords(io)
+    elseif contentsid == 0x9 && name == "TileEntities"
+      tileEntities = NBT._read_tag9(io)
+    else
+      NBT._lut_skip[contentsid](io)
+    end
+  end
+  size_ = abs.(size_)
+  blocks = bitunpack(blockStates, palette, 2, (size_[1], size_[3], size_[2]))
+  position = min.(position, position .+ size_ .- sign.(size_)) # get negativemost corner
+  tile_entities::Array{Union{LittleDict{String}, Nothing}, 3} = fill(nothing, size_...)
+  for te in tileEntities
+    x, y, z = _readtriple(te)
+    tile_entities[x+1, y+1, z+1] = te
+  end
+  return Region(position, blocks, tile_entities)
+#     pos, size_ = _readtriple.((regiontag["Position"], regiontag["Size"]))
+#     pos = min.(pos, pos .+ size_ .- sign.(size_)) # Get negativemost corner
+#     size_ = Int64.(abs.(size_)) # Fix size_ sign
+  
+#     # Array of size `size_`, but reading blocks in XZY order
+#     palette = _AbstractBlockState.(regiontag["BlockStatePalette"])
+#     # compressedBlocks = CompressedPalettedContainer(palette, regiontag["BlockStates"])
+#     # blocks = PooledArray(compressedBlocks, 2, (size_[1], size_[3], size_[2]))
+#     blocks = bitunpack(regiontag["BlockStates"], palette, 2, (size_[1], size_[3], size_[2]))
+#     blocks.refs = permutedims(blocks.refs, (1, 3, 2))
+# 
+#     tile_entities::Array{Union{LittleDict{String}, Nothing}} = fill(nothing, size_...)
+#     for te in regiontag["TileEntities"]
+#       x, y, z = _readtriple(te)
+#       tile_entities[x+1, y+1, z+1] = te
+#     end
+end
+
+function read_coords(io::IO)
+  local x::Int32, y::Int32, z::Int32
+  while (contentsid = read(io, UInt8)) !== 0x0
+    name = NBT._read_name(io)
+    if contentsid == 0x3 && name == "x"
+      x = NBT._read_tag3(io)
+    elseif contentsid == 0x3 && name == "y"
+      y = NBT._read_tag3(io)
+    elseif contentsid == 0x3 && name == "z"
+      z = NBT._read_tag3(io)
+    else
+      NBT._lut_skip[contentsid](io)
+    end
+  end
+  return (x, y, z)
+end
+
+function read_blockstate_palette(io::IO)
+  contentsid = Base.read(io, UInt8)
+  size = ntoh(Base.read(io, Int32))
+  contentsid != 0xa && return AbstractBlockState[]
+  palette = Vector{AbstractBlockState}(undef, size)
+  for i in 1:size
+    palette[i] = read_blockstate(io)
+  end
+  return palette
+end
+
+function read_blockstate(io::IO)
+  local id::String, properties::LittleDict{String, String}
+  has_props = false
+  while (contentsid = Base.read(io, UInt8)) != 0x0
+    name = NBT._read_name(io)
+    if contentsid == 0x8 && name == "Name"
+      id = NBT._read_tag8(io)
+    elseif contentsid == 0xa && name == "Properties"
+      keys = String[]
+      values = String[]
+      while (contentsid1 = Base.read(io, UInt8)) != 0x0
+        if contentsid1 != 0x8
+          NBT._skip_name(io)
+          NBT._lut_skip[contentsid1](io)
+          continue
+        end
+        push!(keys, NBT._read_name(io))
+        push!(values, NBT._read_tag8(io))
+      end
+      properties = LittleDict(keys, values)
+      has_props = true
+    else
+      NBT._lut_skip[contentsid](io)
+    end
   end
 
-  return Litematic(root_tag["MinecraftDataVersion"], root_tag["Metadata"], regions)
+  has_props && return BlockState(id, properties)
+  return Block(id)
 end
+
+# function Base.read(io::IO, ::Type{Litematic})
+#   root_tag = NBT.read(io).second
+#   regiontags = root_tag["Regions"]
+#   regions = Vector{Region}(undef, length(regiontags))
+
+#   for (i, pair) in enumerate(regiontags)
+#     regiontag = pair.second
+#     pos, size_ = _readtriple.((regiontag["Position"], regiontag["Size"]))
+#     pos = min.(pos, pos .+ size_ .- sign.(size_)) # Get negativemost corner
+#     size_ = Int64.(abs.(size_)) # Fix size_ sign
+
+#     # Array of size `size_`, but reading blocks in XZY order
+#     palette = _AbstractBlockState.(regiontag["BlockStatePalette"])
+#     # compressedBlocks = CompressedPalettedContainer(palette, regiontag["BlockStates"])
+#     # blocks = PooledArray(compressedBlocks, 2, (size_[1], size_[3], size_[2]))
+#     blocks = bitunpack(regiontag["BlockStates"], palette, 2, (size_[1], size_[3], size_[2]))
+#     blocks.refs = permutedims(blocks.refs, (1, 3, 2))
+
+#     tile_entities::Array{Union{LittleDict{String}, Nothing}} = fill(nothing, size_...)
+#     for te in regiontag["TileEntities"]
+#       x, y, z = _readtriple(te)
+#       tile_entities[x+1, y+1, z+1] = te
+#     end
+
+#     regions[i] = Region(pair.first, pos, blocks, tile_entities)
+#   end
+
+#   return Litematic(root_tag["MinecraftDataVersion"], root_tag["Metadata"], regions)
+# end
 
 function Base.write(io::IO, litematic::Litematic)
   s, bytes = begin_nbt_file(io)
@@ -179,14 +261,15 @@ function Base.write(io::IO, litematic::Litematic)
 
   for region in litematic.regions
     bytes += begin_compound(s, region.name)
-    bytes += write_tag(s, "BlockStates" => CompressedPalettedContainer(_permutedims(region.blocks, (1, 3, 2)), 2).data)
-    bytes += write_tag(s, "PendingBlockTicks" => TagList())
+    # bytes += write_tag(s, "BlockStates" => CompressedPalettedContainer(_permutedims(region.blocks, (1, 3, 2)), 2))
+    bytes += write_tag(s, "BlockStates" => bitpack(_permutedims(region.blocks, (1, 3, 2)), 2))
+    bytes += write_tag(s, "PendingBlockTicks" => LittleDict{String}[])
     bytes += write_tag(s, "Position" => _writetriple(region.pos))
-    bytes += write_tag(s, "BlockStatePalette" => TagList(_Tag.(region.blocks.pool)))
+    bytes += write_tag(s, "BlockStatePalette" => _Tag.(region.blocks.pool))
     bytes += write_tag(s, "Size" => _writetriple(Int32.(size(region.blocks))))
-    bytes += write_tag(s, "PendingFluidTicks" => TagList())
-    bytes += write_tag(s, "TileEntities" => TagList(TagCompound{Any}[t for t in region.tile_entities if t !== nothing]))
-    bytes += write_tag(s, "Entities" => TagList())
+    bytes += write_tag(s, "PendingFluidTicks" => LittleDict{String}[])
+    bytes += write_tag(s, "TileEntities" => LittleDict{String}[t for t in region.tile_entities if t !== nothing])
+    bytes += write_tag(s, "Entities" => LittleDict{String}[])
     bytes += end_compound(s)
   end
 
@@ -195,27 +278,18 @@ function Base.write(io::IO, litematic::Litematic)
   return bytes
 end
 
-@inline
-function _Tag(block::Block)
-  return TagCompound(["Name" => block.id])
+@inline function _Tag(block::Block)
+  return LittleDict("Name" => block.id)
 end
 
-@inline
-function _Tag(blockState::BlockStateVector)
+@inline function _Tag(blockState::BlockState)
   @inbounds begin
-  return TagCompound(["Name" => blockState.id, "Properties" => TagCompound(blockState.properties)])
+  return LittleDict("Name" => blockState.id, "Properties" => blockState.properties)
 end end
 
-@inline
-function _Tag(blockState::BlockStateDict)
-  @inbounds begin
-  return TagCompound(["Name" => blockState.id, "Properties" => TagCompound(collect(blockState.properties))])
-end end
-
-function _AbstractBlockState(tag::TagCompound) :: AbstractBlockState
-  props = tag["Properties"]
-  props === nothing && return Block(tag["Name"])
-  return BlockStateDict(tag["Name"], Dict(props.data))
+function _AbstractBlockState(tag::LittleDict{String}) :: AbstractBlockState
+  haskey(tag, "Properties") || return Block(tag["Name"])
+  return BlockState(tag["Name"], tag["Properties"])
 end
 
 @inline function _permutedims(p::PooledArray{<:AbstractBlockState, <:Unsigned, 3, Array{UInt32, 3}}, perm::Tuple{Int64, Int64, Int64})
@@ -224,12 +298,12 @@ end
   return p1
 end
 
-@inline function _readtriple(tag::TagCompound{T}) where T
+@inline function _readtriple(tag::LittleDict{String})
   return (tag["x"], tag["y"], tag["z"])
 end
 
 @inline function _writetriple(t::Tuple{Int32, Int32, Int32})
-  return TagCompound(["x" => t[1], "y" => t[2], "z" => t[3]])
+  return LittleDict("x" => t[1], "y" => t[2], "z" => t[3])
 end
 
 end
